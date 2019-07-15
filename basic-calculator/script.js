48eb5c57fe9e4d33155463fe746c2c6c22b01027
vittominacori <vittoriominacori@gmail.com>
2019-07-15 10:24:02 +0200

    feat: using objects for calculator

--- basic-calculator/script.js
+++ basic-calculator/script.js
@@ -1,26 +1,24 @@
-function sum(a, b) {
-  return a + b;
-}
-
-function diff(a, b) {
-  return a - b;
-}
-
-function mul(a, b) {
-  return a * b;
-}
-
-function div(a, b) {
-  return a / b;
-}
-
-function calculate(operator, a, b) {
-  try {
-    console.log(operator(a, b));
-  } catch (e) {
-    console.warn(e);
+const calculator = {
+  sum(a, b) {
+    return a + b;
+  },
+  mul(a, b) {
+    return a * b;
+  },
+  diff(a, b) {
+    return a - b;
+  },
+  div(a, b) {
+    return a / b;
+  },
+  calculate(operator, a, b) {
+    try {
+      return this[operator](a, b);
+    } catch (e) {
+      console.warn(e);
+    }
   }
-}
+};
 
-console.log("Usage: > calculate(sum, 2, 5)");
-calculate(sum, 2, 5);
+console.log("Usage: > calculator.calculate('sum', 2, 5)");
+console.log(calculator.calculate('sum', 2, 5));
